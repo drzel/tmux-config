9217c43ef418e2edff0a0262d61b4948defab7f1
Sheldon Johnson <shayolden@hotmail.com>
2018-09-19 16:43:10 +1000

    Add instruction to install plugins, and auto install tpm

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-echo "Symlinking to home directory"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -fs ${DIR}/.tmux.conf ${HOME}/.tmux.conf
 ln -fs ${DIR}/.tmux.osx.conf ${HOME}/.tmux.osx.conf
 ln -fs ${DIR}/.tmux.linux.conf ${HOME}/.tmux.linux.conf
+echo "Run tmux then `prefix + I` to install plugins
