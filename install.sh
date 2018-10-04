22b638ec6e0450c8b739dfd116f07d2948ed262a
Sheldon Johnson <shayolden@hotmail.com>
2018-10-05 07:52:16 +1000

    Fix install script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -fs ${DIR}/.tmux.conf ${HOME}/.tmux.conf
 ln -fs ${DIR}/.tmux.osx.conf ${HOME}/.tmux.osx.conf
 ln -fs ${DIR}/.tmux.linux.conf ${HOME}/.tmux.linux.conf
-echo "Run tmux then `prefix + I` to install plugins
+echo "Run tmux then <prefix + I> to install plugins"
